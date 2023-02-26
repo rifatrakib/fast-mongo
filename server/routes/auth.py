36bfd1206d6d3adb68550c1d3bb8823042951d21
rifatrakib <Rakib.1508@outlook.com>
2023-02-26 17:53:18 +0600

    send email in background when user signs up

--- server/routes/auth.py
+++ server/routes/auth.py
@@ -1,13 +1,22 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, BackgroundTasks, Request, status
 
 from server.database.user import create_new_user
-from server.models.user import UserResponse, UserSignupRequest
+from server.models.base import MessageResponseSchema
+from server.models.user import UserSignupRequest
+from server.services.email import send_email
 from server.services.validators import Tags
 
 router = APIRouter(prefix="/auth", tags=[Tags.authentication])
 
 
-@router.post("/signup", response_model=UserResponse)
-async def create_user(user: UserSignupRequest):
+@router.post(
+    "/signup",
+    name="auth:signup",
+    summary="Create new user",
+    response_model=MessageResponseSchema,
+    status_code=status.HTTP_201_CREATED,
+)
+async def create_user(request: Request, user: UserSignupRequest, task: BackgroundTasks):
     created_user = await create_new_user(user)
-    return UserResponse(**created_user.dict())
+    task.add_task(send_email, request=request, user=created_user)
+    return MessageResponseSchema(msg="Confirm your email to activate your account")
