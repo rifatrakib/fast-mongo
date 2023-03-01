054fa7fbe2f37b93682b951311d95073d71c6f6d
rifatrakib <Rakib.1508@outlook.com>
2023-03-01 08:24:34 +0600

    added endpoint that activates user when clicked from email

--- server/routes/auth.py
+++ server/routes/auth.py
@@ -1,9 +1,11 @@
 from fastapi import APIRouter, BackgroundTasks, Request, status
 
-from server.database.user import create_new_user
+from server.database.user import activate_user, create_new_user, verify_user_activation
 from server.models.base import MessageResponseSchema
 from server.models.user import UserSignupRequest
 from server.services.email import send_email
+from server.services.exceptions import EntityDoesNotExist
+from server.services.messages import http_exc_404_key_expired
 from server.services.validators import Tags
 
 router = APIRouter(prefix="/auth", tags=[Tags.authentication])
@@ -20,3 +22,19 @@ async def create_user(request: Request, user: UserSignupRequest, task: Backgroun
     created_user = await create_new_user(user)
     task.add_task(send_email, request=request, user=created_user)
     return MessageResponseSchema(msg="Confirm your email to activate your account")
+
+
+@router.get(
+    "/activate-account/{activation_key}",
+    name="auth:activation",
+    summary="Activate user account with random activation key",
+    response_model=MessageResponseSchema,
+    status_code=status.HTTP_202_ACCEPTED,
+)
+async def activation_key(activation_key: str):
+    try:
+        username = await verify_user_activation(activation_key)
+        await activate_user(username)
+    except EntityDoesNotExist:
+        raise await http_exc_404_key_expired()
+    return MessageResponseSchema(msg="Your account has been activated")
