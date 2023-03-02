1e54304703311b12590d49b2ac7d9aa499e3f24f
rifatrakib <Rakib.1508@outlook.com>
2023-03-02 14:04:23 +0600

    add new exception handling functions

--- server/services/messages.py
+++ server/services/messages.py
@@ -6,3 +6,17 @@ async def http_exc_404_key_expired() -> Exception:
         status_code=status.HTTP_404_NOT_FOUND,
         detail={"msg": "Provided key has expired! Please validate before expiration."},
     )
+
+
+async def http_exc_409_conflict(message: str) -> Exception:
+    raise HTTPException(
+        status_code=status.HTTP_409_CONFLICT,
+        detail={"msg": message},
+    )
+
+
+async def http_exc_412_password_mismatch() -> Exception:
+    raise HTTPException(
+        status_code=status.HTTP_412_PRECONDITION_FAILED,
+        detail={"msg": "Passwords does not match!"},
+    )
