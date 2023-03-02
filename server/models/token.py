e04c3c5ed091d3ab78a570db6a45ffab747b246f
rifatrakib <Rakib.1508@outlook.com>
2023-03-02 07:42:22 +0600

    added new schema for token responses

--- server/models/token.py
+++ server/models/token.py
@@ -1,6 +1,6 @@
 from datetime import datetime
 
-from server.models.base import BaseResponse
+from server.models.base import BaseAPI, BaseResponse
 from server.models.user import UserBase
 
 
@@ -11,3 +11,8 @@ class JWTData(BaseResponse, UserBase):
 class JWToken(JWTData):
     exp: datetime
     sub: str
+
+
+class TokenResponseSchema(BaseAPI):
+    token_type: str
+    access_token: str
