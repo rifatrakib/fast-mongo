43474b1ee40593ee071203dda6d6ed462adf4d86
rifatrakib <Rakib.1508@outlook.com>
2023-03-04 14:28:18 +0600

    new custom exceptions for specific cases

--- server/services/exceptions.py
+++ server/services/exceptions.py
@@ -4,3 +4,13 @@ class EntityDoesNotExist(Exception):
 
 class EntityAlreadyExists(Exception):
     """throw an exception when the data already exist in the database."""
+
+
+class PasswordDoesNotMatch(Exception):
+    """throw an exception when the account password does not match the
+    entitiy's hashed password from the database."""
+
+
+class UserNotActive(Exception):
+    """throw an exception when the account has not been activated through
+    email."""
