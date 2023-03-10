cf96e2a09c00fcbbc4c73f80722c1635bd8ec33c
rifatrakib <Rakib.1508@outlook.com>
2023-03-10 11:23:30 +0600

    refactor database query functions

--- server/database/user.py
+++ server/database/user.py
@@ -8,10 +8,15 @@ from server.security.password import password_manager
 from server.services.exceptions import EntityAlreadyExists, EntityDoesNotExist, PasswordDoesNotMatch, UserNotActive
 
 
-async def is_username_available(username: str) -> bool:
+async def read_user_by_username(username: str) -> User:
     user = await User.find(User.username == username).first_or_none()
-    if user:
-        raise EntityAlreadyExists(f"username {username} has already been taken!")
+    if not user:
+        raise EntityDoesNotExist("No such user exists!")
+    return user
+
+
+async def is_username_available(username: str) -> bool:
+    await read_user_by_username(username=username)
     return True
 
 
@@ -38,10 +43,7 @@ async def create_new_user(username: str, email: EmailStr, password: str) -> User
 
 
 async def authenticate_user(username: str, password: str) -> User:
-    user = await User.find(User.username == username).first_or_none()
-
-    if not user:
-        raise EntityDoesNotExist("No such user exists!")
+    user = await read_user_by_username(username=username)
 
     if not user.is_active:
         raise UserNotActive("Account is not active! please activate through email.")
@@ -57,11 +59,7 @@ async def authenticate_user(username: str, password: str) -> User:
 
 
 async def activate_user(username: str) -> bool:
-    user = await User.find(User.username == username).first_or_none()
-
-    if not user:
-        raise EntityDoesNotExist("No such user exists!")
-
+    user = await read_user_by_username(username=username)
     await user.set({User.is_active: True, User.updated_at: datetime.utcnow()})
     return True
 
